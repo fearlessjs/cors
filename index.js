6ae6f144249a80959c00ea1cc443769ab2d49a9f
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-03-18 00:13:49 -0300

    improve index and fix package

--- index.js
+++ index.js
@@ -11,19 +11,40 @@ const ALLOW_METHODS = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 
 const MAX_AGE_SECONDS = 60 * 60 * 24
 
-module.exports = (
-  options = {
-    allowCredentials: true,
-    allowHeaders: ALLOW_HEADERS,
-    allowMethods: ALLOW_METHODS,
-    exposeHeaders: [''],
-    maxAge: MAX_AGE_SECONDS,
-    origin: '*'
-  }
-) => handlers =>
-  Array.isArray(handlers)
-    ? {
-      cors: options,
-      handlers
+module.exports = (configs = {}) => options => {
+  const {
+    origin = '*',
+    maxAge = MAX_AGE_SECONDS,
+    allowMethods = ALLOW_METHODS,
+    allowHeaders = ALLOW_HEADERS,
+    allowCredentials = true,
+    exposeHeaders = []
+  } = configs
+
+  const cors = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    if (allowCredentials) {
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
     }
-    : { cors: options, ...handlers }
+    if (exposeHeaders.length) {
+      res.setHeader('Access-Control-Expose-Headers', exposeHeaders.join(','))
+    }
+
+    const preFlight = req.method === 'OPTIONS'
+    if (preFlight) {
+      res.setHeader('Access-Control-Allow-Methods', allowMethods.join(','))
+      res.setHeader('Access-Control-Allow-Headers', allowHeaders.join(','))
+      res.setHeader('Access-Control-Max-Age', String(maxAge))
+    }
+  }
+
+  return Array.isArray(options)
+    ? {
+      handlers: options,
+      middlewares: [cors]
+    }
+    : {
+      ...options,
+      middlewares: [...options.middleware, cors]
+    }
+}
